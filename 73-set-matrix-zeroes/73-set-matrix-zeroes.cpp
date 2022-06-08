960c35b2290722f1b726236dbb7c4b492dadb663
YASWANTH MUTAKANI <63775530+Yash1heck@users.noreply.github.com>
2022-06-08 19:09:29 +0530

    Time: 28 ms (19.47%), Space: 13.3 MB (54.14%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+         vector<pair<int,int>>v;
+   
+    for(int i=0;i<matrix.size();++i)
+    {
+        for(int j=0;j<matrix[0].size();++j)
+        {
+            if(matrix[i][j]==0)
+            {
+                v.push_back(make_pair(i,j));
+               
+  
+            
+            }
+            
+        }
+    }
+    for(auto&it:v){
+        int x=it.first;
+        int y=it.second;
+        for(int i=0;i<matrix[0].size();++i)
+        {
+            matrix[x][i]=0;
+        }
+        for(int i=0;i<matrix.size();++i)
+        {
+            matrix[i][y]=0;
+        }
+    }
+        
+    
+        
+    }
+};
